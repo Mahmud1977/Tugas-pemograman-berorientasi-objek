0a30ee97f322cecab700fd5fcd33606668db9dd7
Mahmud1977 <mahmud1977.laopo@gmail.com>
2022-10-16 21:42:05 +0700

    PBO Commit

---
+++ tugas4.py
@@ -0,0 +1,23 @@
+# Nama  : Mahmud S Laopo
+# Nim   : 20210801203
+# TUGAS PBO - Sesi 4
+
+def hitungPPN():
+    nilaiA = int(input("Masukan Nilai Pertama: "))
+    nilaiB = int(input("Masukan Nilai Kedua: "))
+    persen = "10"
+    if nilaiA is not None and nilaiB is not None:
+        hasilTotal = int(nilaiA) + int(nilaiB)
+        hasilPersen = (int(hasilTotal) * int(persen) / 100)
+        hasilPPN = int(hasilTotal) - int(hasilPersen)
+        return print(
+            "\n" + 
+            "Nilai pertama: Rp." + str(nilaiA) + "\n" + 
+            "Nilai kedua: Rp."  + str(nilaiB) + "\n" + 
+            "Total hasil: Rp." + str(hasilTotal) + "\n" +
+            "Hasil perhitungan PPN 10%: Rp." + str(hasilPPN) + "\n"
+        )
+    else:
+        print("Nilai tidak boleh kosong!!!")
+
+hitungPPN()
